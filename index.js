703779a1deebe479a1253f0e59c4736843add0b1
Mehow <Kaiser.mehow@gmail.com>
2020-03-19 23:00:03 +0000

    cookies for game/userid persistance

--- index.js
+++ index.js
@@ -1,121 +1,196 @@
+var cookieParser = require('cookie-parser');
 var app = require('express')();
 const crypto = require('crypto');
+app.use(cookieParser());
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-var targets = {}
-
-function makeTargets(userList){
-  users = Object.keys(userList);
-  console.log(users);
+function makeTargets(game){
+  var userList = game.userList;
+  var users = Object.keys(userList);
+  console.log("making targets");
   for(var i=0; i<users.length;i++){
-    console.log(users[i]);
-    targets[users[i]] = users[(i+1)%users.length];
+    game.targets[users[i]] = users[(i+1)%users.length];
+  }
+  game.state = TARGETS_MADE;
+}
+
+function start(game, msgText){
+  game.startTime = Date.now();
+  textParts = msgText.split(" ")
+  if(textParts.length > 1 && !isNaN(parseInt(textParts[1]))){
+    gameLength = 1000*60*parseInt(textParts[1]);
+  }else{
+    gameLength = 1000*60*5;//5 min game by default
+  }
+  console.log("started");
+  game.state = IN_PLAY;
+}
+
+function snipe(game, sniperId){
+  console.log("snipe");
+  //todo: let people vote on wether this is a valid snipe
+  var oldTarget = game.targets[sniperId];
+  var newTarget = game.targets[oldTarget];
+  if(newTarget == sniperId){
+    return true;
+  }else{
+    game.targets[msg.username] = newTarget;
+    return false;
+  }
+}
+
+function maybeRedirectToExistingGame(cookies){
+  // you can only be part of one game at a time
+  // to if user is already in one, keep redirecting
+  // till they leave it
+  if(cookies["gameId"] in games
+    && cookies["userId"] in games.userList){
+      res.redirect(__dirname + '/index.html');
   }
 }
 
 //todo: handle making and joining games here
 
 app.get('/', function(req, res){
+  maybeRedirectToExistingGame(req.cookies);
   res.sendFile(__dirname + '/lobby.html');
 });
 
 var games = {}
 
-function new_game(code){
-  games[code] = {
-    gameStarted: false,
-    targetsMade: false,
-    userList: {},
+//game state
+var NOT_STARTED = "NOT STARTED";
+var TARGETS_MADE = "TARGETS MADE";
+var IN_PLAY = "IN PLAY";
+
+
+function new_game(userList){
+  return {
+    state: NOT_STARTED,
+    // todo: give admin option to remove
+    userList: userList,
+    targets: {},
+    startTime: undefined,
+    gameLength: undefined,
   };
 
 }
 
 app.get('/make', function(req, res){
+  maybeRedirectToExistingGame(req.cookies);
+
   var first_part = crypto.randomBytes(2).toString('hex');
   var second_part = crypto.randomBytes(2).toString('hex');
   // used number of games as a guarantee prevent collisions
   // (even though collisions must be unlikely anyway for the code to provide security)
   var third_part = Object.keys(games).length.toString(16);
   const code = `${first_part}-${second_part}-${third_part}`;
-  new_game(code);
+  games[code] = new_game({});
   res.redirect(`/game/${code}`);
 });
 
 app.get('/join', function(req, res){
-  if(!'code' in req.query){
+  maybeRedirectToExistingGame(req.cookies);
+
+  if('code' in req.query){
     var code = req.query.code;
     res.redirect(`/game/${code}`);
   }else{
-    res.sendFile(__dirname + '/index.html');
+    res.redirect(`/`);
   }
 });
 
 app.get('/game/:code', function(req, res){
+
+  // don't do this
+  // because if user hits/game/:code we should assume they are tyring to
+  // leave an old game and join this one
+  // maybeRedirectToExistingGame(req.cookies);
+
   if(!req.params.code in games){
-    res.redirect(`/make_game`);
+    res.redirect(`/`);
   }else{
+
+    var game = games[req.params.code];
+
+    if(!(req.cookies["userId"] in game.userList)){
+
+      if(game.state != NOT_STARTED){
+        console.log("Attempt to join game " + req.params.code + " that has already started");
+        res.redirect(`/`);
+      }
+
+      var randomness = crypto.randomBytes(256).toString('hex');
+      var uniqueness = Object.keys(game.userList).length;
+      var idToken = `${randomness}-${uniqueness}`;
+
+      game.userList[idToken] = {};
+      // todo: should we sign these? Not much need really
+      // does signing make horizontal scaling more painful?
+      res.cookie("gameId", req.params.code);
+      res.cookie("userId", idToken);
+    }
+
     res.sendFile(__dirname + '/index.html');
   }
 });
 
-
-var gameStarted = false;
-var startTime;
-var gameLength;
-var targetsMade = false;
-
-//todo: remove users when they leave
-//or give admin option to remove
-var userList = {}
-
 io.on('connection', function(socket){
+
+  let gameId = socket.handshake.query.gameId;
+  if(gameId in games){
+    socket.gameId = gameId;
+  }else{
+    console.log("invalid gameid");
+    return;
+  }
+
+  var game = games[gameId];
+
+  let userId = socket.handshake.query.userId;
+  if(userId in game.userList){
+    socket.userId = userId;
+  }else{
+    console.log("invalid userid");
+    return;
+  }
+  
   io.emit('chat message', {'text': 'a user joined'});
   socket.on('chat message', function(msg){
 
-    if(msg.username != '' && !(msg.username in userList)){
-      userList[msg.username] = true;
+    console.log(game.state);
+
+    //todo: save this onto users cookie/userId dict
+    if(msg.username != '' && game.state == NOT_STARTED){
+      game.userList[socket.userId].username = msg.username;
+    }else{
+      console.log("attempted to set username for "  + socket.userId + " to " + msg.username + " but state != NOT_STARTED");
     }
-    msg.userList = userList;
+    msg.userList = game.userList;
     console.log(msg);
-    if(!gameStarted && msg.text == "@maketargets"){
-      console.log("maket");
-      makeTargets(userList);
-      msg.targets = targets;
-      targetsMade = true;
-    }else if(targetsMade && !gameStarted && msg.text.startsWith("@start")){
-      startTime = Date.now();
-      textParts = msg.text.split(" ")
-      if(textParts.length > 1 && !isNaN(parseInt(textParts[1]))){
-        gameLength = 1000*60*parseInt(textParts[1]);
-      }else{
-        gameLength = 1000*60*5;//5 min game by default
-      }
-      console.log("started");
-      gameStarted = true;
-    }else if(gameStarted && msg.text == "@snipe" && msg.username in targets){
-      console.log("snipe");
-      //todo: let people vote on wether this is a valid snipe
-      var oldTarget = targets[msg.username];
-      var newTarget = targets[oldTarget];
-      if(newTarget == msg.username){
-        msg.winner = msg.username;
-        targets = {}
-        gameStarted = false;
-        targetsMade = false;
-      }else{
-        targets[msg.username] = newTarget;
-        msg.targets = targets;
+    if(game.state == NOT_STARTED && msg.text == "@maketargets"){
+      makeTargets(game);
+      msg.targets = game.targets;
+    }else if(game.state == TARGETS_MADE && msg.text.startsWith("@start")){
+      start(game, msg.text);
+    }else if(game.state == IN_PLAY && msg.text == "@snipe"){
+      gameOver = snipe(game, socket.userId);
+      if(gameOver){
+        console.log("winner");
+        games[gameId] = game = new_game(game.userList);
+        msg.winner = socket.userId;
       }
     }
-    //todo: don't rely on messages for checking timing
-    if(gameStarted && startTime + gameLength < Date.now()){
-      gameStarted = false;
-      targetsMade = false;
-      msg.winner = "The relentless passage of time";
-    }else if(gameStarted){
-      msg.timeLeft =  (startTime + gameLength) - Date.now();
+    //todo: don't rely on messages events to check timing
+    if(game.state == IN_PLAY){
+      if(game.startTime + game.gameLength < Date.now()){
+        game.state = NOT_STARTED;
+        msg.winner = "The relentless passage of time";
+      }else{
+        msg.timeLeft =  (game.startTime + game.gameLength) - Date.now();
+      }
     }
     
     io.emit('chat message', msg);
